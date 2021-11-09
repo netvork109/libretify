1410c53829b8a8506e01ad4b1ff42174fe911e61
netvork109 <netvork109@gmail.com>
2021-11-09 17:54:17 +0500

    Adding the services

--- src/login.py
+++ src/login.py
@@ -16,22 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from flask import Flask
+from flask_login import LoginManager
 
-from src.database import db
-from src.admin import admin
-from src.login import login
-
-class Application:
-    def __init__(self) -> None:
-        self.app = Flask(__name__)
-    
-    def setup(self) -> None:
-        # Adding a services
-        db.init_app(self.app)
-        admin.init_app(self.app)
-        login.init_app(self.app)
-
-    def run(self) -> None:
-        self.app.run()
-    
+# Creating the Login manager
+login = LoginManager()
